41821e1edc01d593226c72ebca0262c6571abae3
karahiyo <a.ryuklnm@gmail.com>
2013-12-28 21:15:24 +0900

    can solve problem. - with no sorting, no parentheses

--- lib/make10.rb
+++ lib/make10.rb
@@ -1,7 +1,21 @@
 module Make10
 
+  @ans_format = "%d%c%d%c%d%c%d"
+  @ops = ["+", "-", "*", "/"]
+
   def solve(*inputs)
-    sprintf("%d%c%d%c%d%c%d", inputs[0] , "+", inputs[1], "+", inputs[2], "+", inputs[3])
+    ans = []
+    @ops.each do |o1|
+      @ops.each do |o2|
+        @ops.each do |o3|
+          exp = sprintf(@ans_format, inputs[0] , o1, inputs[1], o2, inputs[2], o3, inputs[3])
+          ans << exp if eval(exp) == 10
+        end
+      end
+    end
+
+    return ans
+
   end
 
   module_function :solve
--- test/test_make10.rb
+++ test/test_make10.rb
@@ -2,7 +2,7 @@ require 'helper'
 
 class Make10Test < Test::Unit::TestCase
   def test_solve
-    ret = Make10.solve(1, 2, 3, 4)
-    assert_equal("1+2+3+4", ret);
+    assert_equal(["1+2+3+4", "1*2*3+4"], Make10.solve(1, 2, 3, 4));
+    assert_equal(["5-1-1+7"], Make10.solve(5, 1, 1, 7));
   end
 end
